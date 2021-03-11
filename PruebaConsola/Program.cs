7ae60593e9d2ccafcc5418a0e0dc01d2ca58d059
Myriam Alvarez <myriamalvarez1006@gmail.com>
2021-03-11 00:14:05 -0300

    setup inicial

---
+++ PruebaConsola/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace PruebaConsola
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
